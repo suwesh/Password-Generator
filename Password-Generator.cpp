5176f736a07376ccb312d60af03924e03149b3df
Suwesh-Sah <83471963+suwesh@users.noreply.github.com>
2022-08-29 19:28:22 +0530

    Update Password-Generator.cpp

--- Password-Generator.cpp
+++ Password-Generator.cpp
@@ -25,21 +25,67 @@ int main()
         cin.ignore();
         cout << "This password is for: ";
         getline(cin, passid);
+        // merge specific word in your password
+
         outfile << "\n";
         outfile << passid << " : ";
-        srand(time(0));
-        for (int i = 0; i < n; i++)
-        {
-            rpass = rand() % passlength + 1;
-            outfile << passelements[rpass];
-        }
-        cout << "Your Password for " << passid << " is created." << endl;
 
-        cout << "Do you need more passwords? \nPress n to discontinue OR Press any key to continue: ";
-        cin >> choice;
-        if (choice == 'n' || choice == 'N')
+        cout << "Do you want to include any specific word in auto generated password? Press y/n" << endl;
+        char word_choice;
+        cin >> word_choice;
+
+        if (word_choice == 'y' || word_choice == 'Y')
         {
-            conti = false;
+            cout << "Enter specific word upto " << n << " length: " << endl;
+            string specific_word;
+            cin >> specific_word;
+
+            int len = specific_word.length();
+            string store = "";
+
+            if (len >= n)
+            {
+                specific_word = specific_word.substr(0, n);
+            }
+
+            srand(time(0));
+            for (int i = 0; i < n - len; i++)
+            {
+                rpass = rand() % passlength + 1;
+                // outfile << passelements[rpass];
+                store += passelements[rpass];
+            }
+
+            // selecting random index for appending the specific word
+            int random_index_for_word = rand() % (n - len + 1);
+            store.insert(random_index_for_word, specific_word);
+            outfile << store;
+
+            cout << "Your Password for " << passid << " is created." << endl;
+
+            cout << "Do you need more passwords? \nPress n to discontinue OR Press any key to continue: ";
+            cin >> choice;
+            if (choice == 'n' || choice == 'N')
+            {
+                conti = false;
+            }
+        }
+        else
+        {
+            srand(time(0));
+            for (int i = 0; i < n; i++)
+            {
+                rpass = rand() % passlength + 1;
+                outfile << passelements[rpass];
+            }
+            cout << "Your Password for " << passid << " is created." << endl;
+
+            cout << "Do you need more passwords? \nPress n to discontinue OR Press any key to continue: ";
+            cin >> choice;
+            if (choice == 'n' || choice == 'N')
+            {
+                conti = false;
+            }
         }
     }
 
